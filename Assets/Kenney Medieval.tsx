ca7df9637c6c6023c092a1f064cf26548020cb77
Stephen <nixon.stephen1@gmail.com>
2020-10-22 16:42:42 -0400

    added more assets and worked on level

---
+++ Assets/Kenney Medieval.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.2" tiledversion="1.3.1" name="Kenney Medieval" tilewidth="70" tileheight="70" tilecount="336" columns="24">
+ <image source="../../../../Desktop/GameAssets/KenneyMedievalPlatformer/Tilesheet/medieval_tilesheet.png" trans="ff00ff" width="1680" height="980"/>
+</tileset>
